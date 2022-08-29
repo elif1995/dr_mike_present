899b469f439411b9900fe5bfaf811d402d6e743d
edimmfs <e21211995@gmail.com>
2022-08-29 16:53:09 +0300

    added loading page

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import {useEffect, useState} from 'react';
 import Navbar from './components/Navbar/navbar.jsx';
 import Header from './components/Header/header.jsx';
 import Main from './components/Main/main.jsx';
@@ -5,12 +6,27 @@ import './App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFacebook ,faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 import {  faPhone} from '@fortawesome/free-solid-svg-icons';
+import Logo from './images/logo.jpg'
+
+
 
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  
+  useEffect(() => {
+    setIsLoading(true);
+    setTimeout(() => {
+      setIsLoading(false)
+    },1500)
+  },[])
+  
   return (
     <div className="App">
       
+      {isLoading ? (<div className="loading-page"><img className="loading-logo" src={Logo} height="140" width="140"/><div className="loading-circle"></div></div >):
+
+      (<>
       <Navbar />
       
       <Header />
@@ -24,6 +40,8 @@ function App() {
         <div className="contact-option fa-2x" id="contact-facebook"><a href="https://www.facebook.com/DrMikeFrenkin"><FontAwesomeIcon icon={faFacebook}></FontAwesomeIcon></a></div>
         <div className="contact-option fa-2x"><a href="tel:08-9353339"><FontAwesomeIcon icon={faPhone}></FontAwesomeIcon></a></div>
       </div>
+      </>)
+    }
     </div>
   );
 }
